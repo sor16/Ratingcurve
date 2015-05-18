2fb8bac2bff26e7f6ad689970441814cfad298fd
axelornj <aoj8@hi.is>
2015-05-18 15:13:34 +0000

    change variable name

--- server.R
+++ server.R
@@ -12,7 +12,7 @@ shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
     
     # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
+    v    <- faithful[, 2] 
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
